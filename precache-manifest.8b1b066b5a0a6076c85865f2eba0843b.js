a9b685e4f95de7082b3ad00257c57570a3d7d215
Chemissi <ghassen.chemissi@gmail.com>
2020-04-09 12:24:47 +0200

    Updates

---
+++ precache-manifest.8b1b066b5a0a6076c85865f2eba0843b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45869d73d9efb18a55c7c00ef6ef793b",
+    "url": "/msit-app/index.html"
+  },
+  {
+    "revision": "564be0871acb9450670f",
+    "url": "/msit-app/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "5b309fbcdb115e4839d7",
+    "url": "/msit-app/static/css/main.b1063904.chunk.css"
+  },
+  {
+    "revision": "564be0871acb9450670f",
+    "url": "/msit-app/static/js/2.2ee950bc.chunk.js"
+  },
+  {
+    "revision": "1fedcf5c84e2f9b04a7e15ecdecddbbe",
+    "url": "/msit-app/static/js/2.2ee950bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b309fbcdb115e4839d7",
+    "url": "/msit-app/static/js/main.75f62baa.chunk.js"
+  },
+  {
+    "revision": "5f4b86e829bb36d81cb0",
+    "url": "/msit-app/static/js/runtime-main.38d4a1d5.js"
+  }
+]);
